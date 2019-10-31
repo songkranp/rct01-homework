4fcf8b50353fa4f11f3e409f45adcee628edc8f7
Songkran Piyangkron <songkranp@msn.com>
2019-10-31 09:52:09 +0700

    day1 homework

---
+++ day1/react-film/src/FilmDetails.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class FilmDetails extends Component {
+
+  render(){
+    return (
+      <div className="film-details">
+        <h1 className="section-title">DETAILS</h1>
+      </div>
+    );
+  }
+
+}
+
+export default FilmDetails; //this lets us 'import FilmDetails from ' ./FilmDetails';
---
+++ day1/react-film/src/FilmListing.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import FilmRow from './FilmRow';
+
+class FilmListing extends Component {
+
+  render(){
+    const allFilms = this.props.films.map((film, index) => ( <FilmRow film={film} key={film.id}/> ))
+    return (
+      <div className="film-list">
+        <h1 className="section-title">FILMS</h1>
+        {allFilms}
+      </div>
+    );
+  }
+
+}
+
+export default FilmListing; //this lets us 'import FilmListing from ' ./FilmListing';
